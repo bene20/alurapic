3a1bd19435ad3317a7f11c43db8f0d4d52883ec4
ebenezer.botelho <ebenezer.botelho@trt10.jus.br>
2019-02-08 16:52:07 -0200

    *) Até aula 03-01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Photo } from './photos/photo/photo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  photos: Photo[] = [
+    { url: 'https://osegredo.com.br/wp-content/uploads/2018/07/signo-de-leao-830x450.jpg', description: 'Leão 1'},
+    { url: 'https://statig1.akamaized.net/bancodeimagens/7k/26/1s/7k261s08f31s2yk4y38m33y0k.jpg', description: 'Leão 2'}
+  ];
 }
