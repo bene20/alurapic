e70e6843b075a46890290edd5e2004806ffc3964
ebenezer.botelho <ebenezer.botelho@trt10.jus.br>
2019-02-11 12:07:14 -0200

    *) Inclusão da api de backend no projeto *) ALteração do script startWorking.sh para carregar a api de backend

--- startWorking.sh
+++ startWorking.sh
@@ -1,3 +1,11 @@
+#ATENÇÃO:
+#Como a versão do node usada neste curso era bem antiga, algumas ações foram necessárias para poder rodar a api:
+#   1) Instalar o gerenciador de versões no node (https://www.npmjs.com/package/n) via comando "sudo npm install -g n"
+#   2) Usar o 'n' para instalar e passar a usar a versão 8.12.0 do node na máquina local via comando "sudo n 8.12.0"
+#      2.1) No meu caso, deu erro de certificado por conta do ambiente de TRT10. Utilizei o comando "sudo update-ca-certificates --fresh" antes do comando acima para resolver isso
+#      2.2) É possível que o npm quebre após alterar a versão do node. Se isso acontecer, corrija o problema com o comando "curl -0 -L https://npmjs.com/install.sh | sudo sh"
+#      2.3) Eu também tive um problema no TRT10 para executar o npm install na api por conta do certificado. Use isso antes do npm install se for o seu caso: "export NODE_TLS_REJECT_UNAUTHORIZED=0"
+
 #!/bin/bash
 . ~/scripts/biblioteca_funcoes.sh # Include da minha biblioteca de funções
 
@@ -16,14 +24,14 @@ tempFile=$(geraArquivoTemporario "screencfgAngular") #Criando o arquivo de confi
 >$tempFile
 
 #NG SERVE
-echo "screen -t NG_SERVE ng serve" >> $tempFile #Crio uma nova sessão na seção 'NG_SERVE'
+echo "screen -t NG_SERVE ng serve" >> $tempFile #Crio uma nova sessão na seção 'NG_SERVE e carrego o serviço do angular'
 echo "zombie k" >> $tempFile #Indico que esta seção deve ser preservada quando finalizar
 echo "split" >> $tempFile #Divido a seção atual horizontalmente
 echo "split -v" >> $tempFile #Divido a seção atual verticalmente
 
-#NODEMON
-echo "focus" >> $tempFile    #Movo o foco para a próxima seção (NODEMON)
-#echo "screen -t NODEMON nodemon --watch backend --exec \"ts-node\" backend/server.ts" >> $tempFile #Crio uma nova sessão na seção 'NODEMON'
+#API BACKEND
+echo "focus" >> $tempFile    #Movo o foco para a próxima seção (API)
+echo "screen -t API bash -c \"cd api && npm start\"" >> $tempFile #Crio uma nova sessão na seção 'API e instancio a API do backend'
 echo "zombie k" >> $tempFile #Indico que esta seção deve ser preservada quando finalizar
 
 #TERMINAL
