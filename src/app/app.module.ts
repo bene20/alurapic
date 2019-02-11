8354f1c81c39e42263886b55a79d4259530b07da
ebenezer.botelho <ebenezer.botelho@trt10.jus.br>
2019-02-11 16:32:12 -0200

    *) Até aula 04-07 - Implementação da primeira rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { HttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { HttpClient } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    PhotosModule
+    PhotosModule,
+    AppRoutingModule
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
