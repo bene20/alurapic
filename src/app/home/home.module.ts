bf610e6cc2e7fd18a77c1f297c57ae7d09138ce7
ebenezer.botelho <ebenezer.botelho@trt10.jus.br>
2019-02-27 11:34:25 -0300

    *) Módulo 2 - Até aula 01.08 - Criando mecanismo de login com redirecionamento de página

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    VmessageModule
+    VmessageModule,
+    RouterModule
   ],
   declarations: [SigninComponent]
 })
